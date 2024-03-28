01291c53b32b54f04c5440c01c2ec36083bbbe40
Jolka <firjola@gmail.com>
2024-03-28 16:26:11 +0100

    feature : user can close modal with ESC

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -7,5 +7,21 @@
   </div>
 </template>
 
-<script></script>
+<script>
+import { onBeforeUnmount } from 'vue';
+export default {
+  setup(props, { emit }) {
+    let onKeydown = (event) => {
+      console.log(event.key);
+      if (event.key == 'Escape') {
+        emit('closeModal');
+      }
+    };
+    window.addEventListener('keydown', onKeydown);
+    onBeforeUnmount(() => {
+      window.removeEventListener('keydown', onKeydown);
+    });
+  },
+};
+</script>
 <style scoped></style>
