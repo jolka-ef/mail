e2b8a70895e79f45cdc197351eb3e6b400ee2ec7
Jolka <firjola@gmail.com>
2024-05-08 15:57:24 +0200

    feature & refactor : create reusable icons for every folder and button

---
+++ src/icons/Archive.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg>
+    <g id="ic-archive-box">
+      <path
+        d="M5.5 8.5A.5.5 0 0 1 6 8h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5Z"
+      ></path>
+      <path
+        fill-rule="evenodd"
+        d="M2 6a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6Zm0-3h12v2H2V3Zm1 3v7a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V6H3Z"
+      ></path>
+    </g>
+  </svg>
+</template>
