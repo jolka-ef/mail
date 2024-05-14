af26fb925de1dcec7f5e35985a4eea7577ab2fb0
Jolka <firjola@gmail.com>
2024-05-14 19:06:35 +0200

    fix : move to main.css common button styles

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -6,11 +6,6 @@
 <style scoped>
 button {
   display: flex;
-  background-color: transparent;
-  border: none;
-  font-size: 1em;
-  cursor: pointer;
-  padding: 0;
 }
 button:disabled {
   cursor: auto;
